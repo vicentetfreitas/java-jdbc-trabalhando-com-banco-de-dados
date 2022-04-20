b4694085a033a7e91dc2a0cc1f8555ef64274b9f
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-04-19 23:12:04 -0300

    Insert com parâmetros

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,21 +11,22 @@ public class TestaInsercaoComParametro {
 		String nome = "Mouse'";
 		String descricao = "Mouse sem fio); delete from produto";
 		ConnectionFactory cf = new ConnectionFactory();
-		ConnectionFactory factory = new ConnectionFactory();
-		Connection connection = factory.recuperarConexao();
+		Connection connection = cf.recuperarConexao();
 
-		Statement stm = connection.createStatement();
-		stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('Mouse', 'Mouse sem fio')"
-				, Statement.RETURN_GENERATED_KEYS);
+		PreparedStatement pstm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+				Statement.RETURN_GENERATED_KEYS);
 
-		ResultSet rst = stm.getGeneratedKeys();
-		while(rst.next()) {
+		pstm.setString(1, nome);
+		pstm.setString(2, descricao);
+		pstm.execute();
+
+		ResultSet rst = pstm.getGeneratedKeys();
+
+		while (rst.next()) {
 			Integer id = rst.getInt(1);
 			System.out.println("O id criado foi: " + id);
 		}
 		rst.close();
-		stm.close();
-		connection.close();
+
 	}
 }
-
